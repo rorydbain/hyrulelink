4c3813012053cc9d7e0abf2e8e4787e0e81cc0b7
Rory Bain <rory.bain@fanduel.com>
2018-11-12 23:15:07 +0000

    Updating git ingore

---
+++ HyruleLink/HyruleLink/TableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCell.swift
+//  HyruleLink
+//
+//  Created by Rory Bain on 12/11/2018.
+//  Copyright © 2018 RoryBain. All rights reserved.
+//
+
+import Foundation
---
+++ HyruleLink/Link.swift
@@ -0,0 +1,9 @@
+//
+//  Link.swift
+//  HyruleLink
+//
+//  Created by Rory Bain on 12/11/2018.
+//  Copyright © 2018 RoryBain. All rights reserved.
+//
+
+import Foundation
