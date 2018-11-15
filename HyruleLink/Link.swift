5fd66567c8e679a06edfd76f6fa2bc4ed6d7cffc
Rory Bain <rory.bain@fanduel.com>
2018-11-15 21:05:14 +0000

    Tidying link, preparing to make use case

--- HyruleLink/Link.swift
+++ HyruleLink/Link.swift
@@ -45,38 +45,41 @@ class Link: Object {
     // weird but hey ho
     func populateParameters(presentingAlertFrom viewController: UIViewController, completed: @escaping (LinkInfo) -> Void) {
         let paramRegex = "\\$\\{(.*?)\\}" // e.g. /pickem/${tournamentId}
-        let matches = try! NSRegularExpression(pattern: paramRegex).matches(in: path, options: [], range: NSRange(path.startIndex..., in: path))
+        let matches = try! NSRegularExpression(pattern: paramRegex)
+            .matches(in: path, options: [], range: NSRange(path.startIndex..., in: path))
         
-        var matchesToUserInput = [(NSTextCheckingResult, String)]()
-        Observable.from(matches)
-            .concatMap { [weak self] match -> Observable<(NSTextCheckingResult,String)> in
-                guard let strongSelf = self else { return Observable.empty() }
-                return strongSelf.getParam(forMatch: match, viewController: viewController)
-            }.scan([(NSTextCheckingResult, String)](), accumulator: { (total, arg1) in
-                let (match, userReplacement) = arg1
-                var mutableArray = total
-                mutableArray.append((match, userReplacement))
-                return mutableArray
-            }).subscribe(onNext: { dict in
-                matchesToUserInput = dict
-            }, onError: nil,
-               onCompleted: { [weak self] in
+        Observable
+            .from(matches)
+            .concatMap { [weak self] match -> Observable<(RegexReplacement)> in
+                guard
+                    let path = self?.path,
+                    let strongSelf = self,
+                    let swiftRange = Range(match.range, in: path) else { return Observable.empty() }
+                
+                let viewModel = TextFieldAlertViewModel(title: "Enter value for \(String(path[swiftRange]))", message: nil)
+                return strongSelf
+                    .show(textFieldAlertViewModel: viewModel, from: viewController)
+                    .map { textInput in RegexReplacement(checkingResult: match, replacement: textInput) }
+            }
+            .toArray()
+            .subscribe(onNext: { [weak self] matchesToUserInput in
                 guard let strongSelf = self else { return }
                 let output = strongSelf.replaceChunks(forValues: matchesToUserInput)
                 completed(output)
-            }).disposed(by: disposeBag)
+            })
+            .disposed(by: disposeBag)
         
         
     }
     
-    private func replaceChunks(forValues matchesToUserInput: [(NSTextCheckingResult, String)]) -> LinkInfo {
+    private func replaceChunks(forValues matchesToUserInput: [RegexReplacement]) -> LinkInfo {
         guard !matchesToUserInput.isEmpty else { return LinkInfo(fullPath: path, replacements: [:]) }
         
         var output = ""
         var previousMatch: NSTextCheckingResult?
         var replacements = [String: String]()
-        for (i, args) in matchesToUserInput.enumerated() {
-            let (match, replacement) = args
+        for (i, regexReplacement) in matchesToUserInput.enumerated() {
+            let (match, replacement) = (regexReplacement.checkingResult, regexReplacement.replacement)
             let currentMatchRange = Range(match.range, in: path)!
             
             if let previousMatch = previousMatch {
@@ -103,24 +106,36 @@ class Link: Object {
         return LinkInfo(fullPath: output, replacements: replacements)
     }
     
-    private func getParam(forMatch match: NSTextCheckingResult, viewController: UIViewController) -> Observable<(NSTextCheckingResult,String)> {
-        let arg = String(path[Range(match.range, in: path)!])
-        return Observable.create({ observer in
-            let alert = UIAlertController(title: "Enter value for \(arg)", message: nil, preferredStyle: .alert)
-            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in observer.onCompleted() })
-            let next = UIAlertAction(title: "Next", style: .default, handler: { _ in
-                let text = alert.textFields![0].text!
-                observer.onNext((match, text))
-                observer.onCompleted()
-            })
-            alert.addTextField { _ in }
-            alert.addAction(cancel)
-            alert.addAction(next)
+    struct TextFieldAlertViewModel {
+        let title: String?
+        let message: String?
+    }
+    
+    private func show(textFieldAlertViewModel: TextFieldAlertViewModel, from viewController: UIViewController) -> Observable<String> {
+        return Observable.create { observer in
+            let alert = UIAlertController(title: textFieldAlertViewModel.title,
+                                          message: textFieldAlertViewModel.message,
+                                          preferredStyle: .alert)
+            
+            alert.addAction(.init(title: "Cancel",
+                                  style: .cancel,
+                                  handler: { _ in
+                                    observer.onCompleted() }))
+            
+            alert.addAction(.init(title: "Submit",
+                                  style: .default,
+                                  handler: { _ in
+                                    observer.onNext(alert.textFields?.first?.text ?? "")
+                                    observer.onCompleted() }))
+            
+            alert.addTextField(configurationHandler: { _ in })
+            
             viewController.present(alert, animated: true, completion: nil)
+            
             return Disposables.create {
                 alert.dismiss(animated: true, completion: nil)
             }
-        })
+        }
     }
     
 }
@@ -128,4 +143,9 @@ class Link: Object {
 struct LinkInfo {
     let fullPath: String
     let replacements: [String: String]
+}
+
+struct RegexReplacement {
+    let checkingResult: NSTextCheckingResult
+    let replacement: String
 }
